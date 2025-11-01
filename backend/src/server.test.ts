e88de8e5a6347b99ebf55dba342a260ca357beff
Nua <tanadhpianpark@gmail.com>
2025-11-02 01:55:57 +0700

    Add sign-up and sign-in user auth tests

--- backend/src/server.test.ts
+++ backend/src/server.test.ts
@@ -16,3 +16,52 @@ describe("GET unknown path", () => {
       .expect(404);
   });
 });
+
+describe("POST /auth/password/sign-up", function() {
+  it("should return 201 Created", function(done) {
+    request(app)
+      .post("/auth/password/sign-up")
+      .auth("testuser@gmail.com", "123456789")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(201, done);
+  });
+
+  it("should return 400 Bad Request", function(done) {
+    request(app)
+      .post("/auth/password/sign-up")
+      .auth("", "")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(400, done);
+  });
+});
+
+describe("POST /auth/password/sign-in", function() {
+  it("should return 200 OK", function(done) {
+    request(app)
+      .post("/auth/password/sign-in")
+      .auth("testuser@gmail.com", "123456789")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200, done);
+  });
+
+  it("should return 400 Bad Request", function(done) {
+    request(app)
+      .post("/auth/password/sign-in")
+      .auth("", "")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(400, done);
+  });
+
+  it("should return 401 Unauthorized", function(done) {
+    request(app)
+      .post("/auth/password/sign-in")
+      .auth("userdoesntexist@gmail.com", "123456789")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(401, done);
+  });
+});
